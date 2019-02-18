20ee40e58d14f103ec15758c5fae73889309250d
Kai Renken <s_n5rv8u@agkozlov07.math.uni-bremen.de>
2019-02-18 10:33:54 +0100

    Splitted header / source directories

---
+++ sources/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include "../headers/Matrix.h"
+#include "../headers/basics.h"
+
+int main()
+{
+    Matrix* cochain = new Matrix(6, 1);
+
+    cochain->setEntry(0,0,1);
+    cochain->setEntry(1,0,0);
+    cochain->setEntry(2,0,0);
+    cochain->setEntry(3,0,0);
+    cochain->setEntry(4,0,0);
+    cochain->setEntry(5,0,1);
+
+    std::cout << cochain->getCosystolicNorm(4,1) << std::endl;
+
+    delete cochain;
+
+	return 0;
+}
